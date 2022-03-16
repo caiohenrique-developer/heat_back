4cccdbcf4872b16c69dfa1e63cd0f16c45c9e10c
caiohenrique-developer <caiorussellrox@msn.com>
2022-03-16 16:05:07 -0300

    ✨ [feat]: Add Websocket to application

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,17 @@
 import "dotenv/config"
 import express from 'express';
+import http from "http"
+import cors from "cors"
+import { Server } from "socket.io"
 import { router } from './routes'
 
 const app = express();
+app.use(cors())
+
+const serverHttp = http.createServer(app);
+const io = new Server(serverHttp, { cors: { origin: "*" }});
+
+io.on("connection", (socket) => console.log(`Usuário conectado no socket: ${socket.id}`))
 
 app.use(express.json())
 app.use(router)
@@ -17,4 +26,4 @@ app.get('/signin/callback', (req, res) => {
     return res.json({ code });
 })
 
-app.listen(4000, () => console.log('Server is running on port 4000 🛸'));
+export { serverHttp, io }
